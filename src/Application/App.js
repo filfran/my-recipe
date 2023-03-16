7a08b1241a116411c829f7bd9e24aef31a495b2f
filfran <felipefran13@gmail.com>
2023-03-16 15:58:58 +0200

    url integration

--- src/Application/App.js
+++ src/Application/App.js
@@ -4,6 +4,7 @@ import Header from "Layouts/Header";
 import SearchBar from "Layouts/SearchBar";
 
 import { createTheme, ThemeProvider } from "@mui/material";
+import axios from "axios";
 
 import styles from "./App.module.scss";
 
@@ -23,6 +24,8 @@ const theme = createTheme({
 
 function App() {
   const { main } = styles;
+  const url = process.env.REACT_APP_RECIPES_URL;
+  const apiKey = process.env.REACT_APP_API_KEY;
 
   const [searchValue, setSearchValue] = useState("");
 
@@ -30,10 +33,16 @@ function App() {
     setSearchValue(e.target.value);
   };
 
-  const searchHandler = (e) => {
-    e.preventDefault();
-    console.log("searching...");
-    console.log(searchValue);
+  const searchHandler = async (e) => {
+    try {
+      e.preventDefault();
+      const response = await axios.get(
+        `${url}?apiKey=${apiKey}&query=${searchValue}`
+      );
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
